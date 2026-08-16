b34364f91ab1ba7896a5264e3c80563f42d19fc7
PorcupineAI <okerekevip@gmail.com>
2026-08-16 09:38:23 +0100

    Update and rename backend/schemas.py to backend/schemas/_init_.py

---
+++ backend/schemas/_init_.py
@@ -0,0 +1,4 @@
+from .site import SiteBase, SiteCreate, SiteResponse
+from .gender import GenderMetricsCreate, GenderMetricsResponse, GenderPolicyComplianceResponse
+from .regulatory import RegulatoryMatrixResponse
+from .optimization import OptimizationInput, OptimizationResponse
